470a1f39426f34d246029479044d195e2a16fde1
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-06-21 18:15:51 -0400

    setting up the basics with TODO

---
+++ assets/java/script.js
@@ -0,0 +1,22 @@
+//GOAL: I am taking a quiz that will regenerate after each question in answered, and will reduce the timer by one second each time the user incorrectly answers a question.
+
+
+//TODO: create a click to start button that will start the timer, and take the user to the next screen.
+
+
+//TODO: Figure out how to refresh the page to a new page each time the user clicks a response.
+
+
+//TODO: Figure out the mathmatics of when user clicks this then this etc. 
+
+
+//TODO: When a timer starts, presented with a question
+
+
+//TODO: answer a question presented with another question
+
+
+//TODO: answer a question incorrectly time is subtracted from the clock
+
+
+//TO DO: all questions are answered or the timer reaches 0, the game is over, then the user enters initials to save score and can see the tally.
