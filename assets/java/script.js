6d36459490fac02116497284d862a7235b8be155
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-06-25 13:13:34 -0400

    adding some java, from HTML

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,14 +1,58 @@
 //GOAL: I am taking a quiz that will regenerate after each question in answered, and will reduce the timer by one second each time the user incorrectly answers a question.
 
+var timer = document.querySelector(".timer");
 
+
+console.log(timer);
 
 //TODO: create a click to start button that will start the timer, and take the user to the next screen.
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click",function(){
+    var quizBox= document.querySelector(".quizbox");
+    quizBox.removeAttribute("class", "hide");
+    var rulesEl= document.querySelector(".rules");
+    rulesEl.setAttribute("class", "hide");
+    questionCard();
+})
 
-var questionOne = [];
-var questionTwo = [];
-var questionThree = [];
-var questionFour = [];
+var questions = [
+    {
+        question: "Question 1 What is the correct HTML element for the largest heading:",
+        choices: ["1", "2", "3", "4"],
+        anwser: "1",
+
+    },
+    {
+        question: "Question 2",
+        choices: ["1", "2", "3", "4"],
+        anwser: "1",
+
+    },
+    {
+        question: "Question 3",
+        choices: ["1", "2", "3", "4"],
+        anwser: "1",
+
+    },
+    {
+        question: "Question 4",
+        choices: ["1", "2", "3", "4"],
+        anwser: "1",
+
+    }
+];
+
+function questionCard(){
+    var currentquestion = questions[0].question
+   var questionEl = document.querySelector(".question1");
+   questionEl.innerHTML="<h2 id='question-number'>" + currentquestion +" </h2>" 
+    questions[0].choices.forEach(function(choice){
+        var button= document.createElement("button");
+    })
+}
+
+var questionNumber = document.querySelector("#question-number");
+
 
 //TODO: Figure out how to refresh the page to a new page each time the user clicks a response.
 
