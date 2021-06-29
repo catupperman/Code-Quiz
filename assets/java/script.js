4b5b0c37146783077be0f2517a8c37b9d8475880
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-06-28 22:01:50 -0400

    Editing timer and second HTML

--- assets/java/script.js
+++ assets/java/script.js
@@ -3,19 +3,23 @@
 var timer = document.querySelector(".timer");
 var secondsLeft = 90;
 var score = 0;
+var questionIndex = 0;
+
+
 
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timer.textContent = secondsLeft + " time left";
-
         if (secondsLeft === 0) {
+            //enter in initials: #msg in HTML
             clearInterval(timerInterval);
         }
     }, 1000
     )
 }
 
+var initials = document.querySelector(".initials");
 
 
 //TODO: create a click to start button that will start the timer, and take the user to the next screen.
@@ -24,13 +28,10 @@ var quizBox = document.querySelector(".quizbox");
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function () {
     setTime(),
-        quizBox.removeAttribute("class", "hide");
+    quizBox.removeAttribute("class", "hide");
     var rulesEl = document.querySelector(".rules");
     rulesEl.setAttribute("class", "hide");
-
     questionCard();
-
-
 }
 )
 
@@ -45,8 +46,7 @@ var questions = [
     },
     {
         question: "Question 2 What does CCS Stand for?",
-        //TODO:open ended response, text box for choice.
-        choices: [""],
+        choices: ["Cascading Style Sheet", "Cascading Standing Sheet", "Crazy Silly Sheet", "Cascading Style Shore"],
         anwser: "Cascading Style Sheet",
 
     },
@@ -68,38 +68,44 @@ var questions = [
 //TODO: answer a question presented with another question
 function questionCard() {
     //render the question
-    var currentquestion = questions[0].question
+    var currentquestion = questions[questionIndex].question
     var questionEl = document.querySelector(".question1");
     questionEl.innerHTML = "<h2 id='question-number'>" + currentquestion + " </h2>";
 
     //render the choices
 
-    questions[0].choices.forEach(function (choice) {
+    questions[questionIndex].choices.forEach(function (choice) {
         var button = document.createElement("button");
         button.textContent = choice;
         button.setAttribute("value", choice);
         button.onclick = function changeContent() {
-            if (choice === questions[0].anwser) {
+            if (this.value === questions[questionIndex].anwser) {
+                questionEl.removeAttribute("h2", "hide");
+                score++;
             } else {
+                //TODO: answer a question incorrectly time is subtracted from the clock
                 timer--;
             }
-            questionEl.removeAttribute("class", "hide");
-            buttton.removeAttribute("button", "hide");
+            questionIndex++;
         }
         quizBox.appendChild(button);
     });
-
 };
-
-
 
 var questionNumber = document.querySelector("#question-number");
 
-//TODO: answer a question incorrectly time is subtracted from the clock
 
 
 
 //TO DO: all questions are answered or the timer reaches 0, the game is over, then the user enters initials to save score and can see the tally.
+//initials box, saved to local storage on Highscores.html
+//
+//var finalScore = score, + initials;
+//function renderMessage (){
+    //finalScore
+    //localStorage .setItem("initials", JSON.stringify(initials))
+    //renderMessage();
+//}
 
 
 
